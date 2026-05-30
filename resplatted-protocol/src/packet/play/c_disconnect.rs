5e9d04acc2d425757259634447df51f7c8da8ca4
ht06 <hthug06@gmail.com>
2026-05-30 19:26:00 +0200

    fix: display a string when disconnecting because of nbt

--- resplatted-protocol/src/packet/play/c_disconnect.rs
+++ resplatted-protocol/src/packet/play/c_disconnect.rs
@@ -1,6 +1,5 @@
-use crate::io::read::MinecraftReadExt;
 use crate::packet::PacketRead;
-use std::io::Cursor;
+use std::io::{Cursor, Read};
 
 pub struct PlayDisconnectPacket {
     pub reason: String,
@@ -10,8 +9,19 @@ impl PacketRead for PlayDisconnectPacket {
     const ID: i32 = 0x20;
 
     fn read(cursor: &mut Cursor<&[u8]>) -> std::io::Result<Self> {
+        let mut buf: Vec<u8> = Vec::new();
+        cursor.read_to_end(&mut buf)?;
+
+        // Only keep real char, not nbt things
+        // TODO: use a nbt reader
+        let clean_reason: String = buf
+            .into_iter()
+            .filter(|&b| b >= 32 && b <= 126) // ASCII table
+            .map(|b| b as char)
+            .collect();
+
         Ok(Self {
-            reason: cursor.read_string()?,
+            reason: clean_reason,
         })
     }
 }
