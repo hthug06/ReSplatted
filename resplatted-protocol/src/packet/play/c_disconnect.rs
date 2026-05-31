522713a4069c448b7edcfaafe756e6c1db187393
ht06 <hthug06@gmail.com>
2026-05-31 21:47:38 +0200

    feat: read nbt when disconnecting

--- resplatted-protocol/src/packet/play/c_disconnect.rs
+++ resplatted-protocol/src/packet/play/c_disconnect.rs
@@ -1,6 +1,6 @@
 use crate::packet::PacketRead;
+use fastnbt::Value;
 use std::io::{Cursor, Read};
-
 pub struct PlayDisconnectPacket {
     pub reason: String,
 }
@@ -12,16 +12,28 @@ impl PacketRead for PlayDisconnectPacket {
         let mut buf: Vec<u8> = Vec::new();
         cursor.read_to_end(&mut buf)?;
 
-        // Only keep real char, not nbt things
-        // TODO: use a nbt reader
-        let clean_reason: String = buf
-            .into_iter()
-            .filter(|&b| (32..=126).contains(&b)) // ASCII table
-            .map(|b| b as char)
-            .collect();
+        // If the first byte us 10, it's a TAG_Compound, so it's a nbt, we inject an empty name for fastnbt to read it
+        let parsed_reason: String = if buf.first() == Some(&0x0A) {
+            buf.insert(1, 0x00); // first byte length = 0
+            buf.insert(2, 0x00); // second byte length = 0
+
+            match fastnbt::from_bytes::<Value>(&buf) {
+                Ok(nbt) => {
+                    format!("{:?}", nbt)
+                }
+                Err(e) => format!("NBT Error: {}", e),
+            }
+        } else {
+            let clean_reason: String = buf
+                .into_iter()
+                .filter(|&b| (32..=126).contains(&b)) // ASCII table
+                .map(|b| b as char)
+                .collect::<String>();
+            clean_reason
+        };
 
         Ok(Self {
-            reason: clean_reason,
+            reason: parsed_reason,
         })
     }
 }
